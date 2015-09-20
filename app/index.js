a16f63516c2e37bbd8117e2ddc709e1c897a1c10
Matthias Blomme <matthiasblomme@gmail.com>
2015-09-20 15:34:13 +0200

    Init: added basic prompting

--- app/index.js
+++ app/index.js
@@ -2,9 +2,67 @@
 
 var generators = require('yeoman-generator');
 var mkdirp = require('mkdirp');
+var yosay = require('yosay')
 
 module.exports = generators.Base.extend({
-	createProjectFileSystem: function () {
+	// YEOMAN RUN LOOP
+	//------------------------------------------------------------------------
+	initializing: function () {
+		this.log(yosay('Allo Allo!, Welcome to WebSMACSS!'));
+	},
+
+	prompting: function () {
+		this._prompt();
+	},
+
+	configuring: function () {
+		this.config.save();
+	},
+
+	writing: function () {
+		this._createProjectFileSystem();
+	},
+
+	install: function () {
+		this.bowerInstall();
+		this.npmInstall();
+	},
+	//-------------------------------------------------------------------------
+	
+	_prompt: function (answers) {
+		this.prompt(this._askQuestions(), function (answers) {
+			this.gitUser = answers.gitUser;
+			this.appname = answers.name;
+			this.appauthor = answers.userName;
+			this.applicense = answers.licenseType;
+			this.log(answers.name);
+		}.bind(this));
+	},
+
+	_askQuestions: function () {
+		var prompts = [
+			{
+				name: 'name',
+				message: 'What is the name for your project?',
+				default: this.appname
+			},
+			{
+				name: 'gitUser',
+				message: 'What is your github username?',
+			},
+			{
+				name: 'userName',
+				message: 'What is your name?',
+			},
+			{
+				name: 'licenceType',
+				message: 'How is your project licensed?',
+				default: 'MIT'
+			}
+		];
+		return prompts;
+	},
+	_createProjectFileSystem: function () {
 		var dstRoot = this.destinationRoot(),
 			srcRoot = this.sourceRoot(),
 			appDir = dstRoot + "/app";
@@ -12,7 +70,12 @@ module.exports = generators.Base.extend({
 		this._createFolderStructure(appDir);
 		this._createStarterFiles(srcRoot, dstRoot);
 	},
-	
+
+	installDependencies: function () {
+		this.bowerInstall();
+		this.npmInstall();
+	},
+
 	_createFolderStructure: function (target) {
 		mkdirp(target + '/wwwroot/html');
 		mkdirp(target + '/wwwroot/scripts');
@@ -28,6 +91,7 @@ module.exports = generators.Base.extend({
 		this.fs.copy(srcRoot + "/.bowerrc", dstRoot + "/.bowerrc");
 		this.fs.copy(srcRoot + "/.jshintrc", dstRoot + "/.jshintrc");
 		this.fs.copy(srcRoot + "/bower.json", dstRoot + "/bower.json");
+		this.fs.copy(srcRoot + "/package.json", dstRoot + "/package.json");
 		this.fs.copy(srcRoot + "/robots.txt", dstRoot + "/app/robots.txt");
 		this.fs.copy(srcRoot + "/humans.txt", dstRoot + "/app/humans.txt");
 		this.fs.copy(srcRoot + "/humans.txt", dstRoot + "/app/.gitignore");
