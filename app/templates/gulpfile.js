1aed4e8c103f9da5ecf9209932c415edb5208966
mblomme <matthiasblomme@gmail.com>
2015-09-24 16:22:22 +0200

    GulpTaks: An auto watch is included for scss files. The output file is called main.css and a sourcemaps is added in css/maps

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -2,19 +2,30 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	uglify = require('gulp-uglify'),
 	sass   = require('gulp-sass'),
-    concat = require('gulp-concat');
+        concat = require('gulp-concat'),
+        sourcemaps = require('gulp-sourcemaps'),
+        plumber = require('gulp-plumber');
 	
 gulp.task('build-css', function() {
-  return gulp.src('/app/scss/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('/app/wwwroot/css'));
-});	
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(plumber({
+        handleError: function (err) {
+            console.log(err);
+            this.emit('end');
+        }
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+  	file: 'app/wwwroot/css/main.css'
+    }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('app/wwwroot/css'));
+});		
 
 gulp.task('watch', function() {
-  gulp.watch('/app/scss/*.scss', ['build-css']);
+  gulp.watch('app/scss/**/*.scss', ['build-css']);
 });
 	
 gulp.task('default', ['watch'], function() {
 	
 });
-
